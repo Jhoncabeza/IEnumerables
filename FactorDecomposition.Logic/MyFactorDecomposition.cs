1076877bc9a26f2858ca6f569eddf5b81545a83e
Jhon Cabeza <cabejhon7@gmail.com>
2023-05-19 16:49:26 -0500

    Documentation and class completion MyFactorDescomposition

---
+++ FactorDecomposition.Logic/MyFactorDecomposition.cs
@@ -0,0 +1,58 @@
+﻿namespace FactorDecomposition.Logic
+{
+    public class MyFactorDecomposition
+    {
+        public int Number { get; set; }
+        public List<int> ListNumber { get; set; }
+        public MyFactorDecomposition(int number)
+        {
+            Number = number;
+            ListNumber = GetPrimeFactors(number);
+        }
+        public override string ToString()
+        {
+            var output = string.Empty;
+
+            for (int i = 0; i < ListNumber.Count; i++)
+            {
+                output += $"{ListNumber[i]}";
+
+                if (i + 1 < ListNumber.Count)
+                {
+                    output += $" x ";
+                }
+
+            }
+            return $"{Number} = {output}";
+        }
+        public static List<int> GetPrimeFactors(int number)
+        {
+            List<int> factors = new();
+            // Check divisibility by 2
+            while (number % 2 == 0)
+            {
+                factors.Add(2);
+                number /= 2;
+            }
+
+            // Check divisibility by odd numbers
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                while (number % i == 0)
+                {
+                    factors.Add(i);
+                    number /= i;
+                }
+            }
+
+            // If the number is prime and greater than 2
+            if (number > 2)
+            {
+                factors.Add(number);
+            }
+            return factors;
+        }
+
+
+    }
+}
---
+++ FactorDecomposition.UI/Program.cs
@@ -0,0 +1,8 @@
+﻿using FactorDecomposition.Logic;
+
+Console.WriteLine("35. DESCOMPOSICION EN FACTORES");
+Console.Write("Ingrese el número a descomponer:");
+var number =  Convert.ToInt32(Console.ReadLine());
+var factor = new MyFactorDecomposition(number);
+
+Console.WriteLine(factor);
